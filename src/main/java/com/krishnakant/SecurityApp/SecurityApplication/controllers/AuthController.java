64168432952f0e185bd3b0027dca4491ccbe240c
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2025-08-10 22:44:44 +0530

    JWT Refresh token and Access token based authentication

--- src/main/java/com/krishnakant/SecurityApp/SecurityApplication/controllers/AuthController.java
+++ src/main/java/com/krishnakant/SecurityApp/SecurityApplication/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.krishnakant.SecurityApp.SecurityApplication.controllers;
 
 import com.krishnakant.SecurityApp.SecurityApplication.dto.LoginDto;
+import com.krishnakant.SecurityApp.SecurityApplication.dto.LoginResponseDto;
 import com.krishnakant.SecurityApp.SecurityApplication.dto.SignUpDto;
 import com.krishnakant.SecurityApp.SecurityApplication.dto.UserDto;
 import com.krishnakant.SecurityApp.SecurityApplication.services.AuthService;
@@ -10,11 +11,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final UserService userService;
     private final AuthService authService;
+
+    @Value("${deploy.env}")
+    private String deployEnv;
 
     @PostMapping("/signup")
     public ResponseEntity<UserDto> signUp(@RequestBody SignUpDto signUpDto){
@@ -31,15 +39,30 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    private ResponseEntity<String> login(@RequestBody LoginDto loginDto, HttpServletRequest request,
+    private ResponseEntity<LoginResponseDto> login(@RequestBody LoginDto loginDto, HttpServletRequest request,
                                          HttpServletResponse response){
-        String token = authService.login(loginDto);
+        LoginResponseDto loginResponseDto = authService.login(loginDto);
 
-        Cookie cookie = new Cookie("token", token);
+        Cookie cookie = new Cookie("refreshToken", loginResponseDto.getRefreshToken());
         cookie.setHttpOnly(true);
 
+        cookie.setSecure("production".equals(deployEnv));
         response.addCookie(cookie);
 
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(loginResponseDto);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDto> refresh(HttpServletRequest request){
+
+//        fetch refresh token from cookies
+        String refreshToken = Arrays.stream(request.getCookies())
+                .filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElseThrow(() -> new AuthenticationServiceException("Refresh token not found inside the Cookies"));
+
+        LoginResponseDto loginResponseDto = authService.refreshToken(refreshToken);
+        return ResponseEntity.ok(loginResponseDto);
     }
 }
--- src/main/java/com/krishnakant/SecurityApp/SecurityApplication/services/AuthService.java
+++ src/main/java/com/krishnakant/SecurityApp/SecurityApplication/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.krishnakant.SecurityApp.SecurityApplication.services;
 
 import com.krishnakant.SecurityApp.SecurityApplication.dto.LoginDto;
+import com.krishnakant.SecurityApp.SecurityApplication.dto.LoginResponseDto;
 import com.krishnakant.SecurityApp.SecurityApplication.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,12 +15,24 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final UserService userService;
 
-    public String login(LoginDto loginDto) {
+    public LoginResponseDto login(LoginDto loginDto) {
         Authentication authentication =  authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDto.getEmail(), loginDto.getPassword())
         );
         User user = (User) authentication.getPrincipal();
-        return jwtService.generateToken(user);
+        String accessToken =  jwtService.generateAccessToken(user);
+        String refreshToken = jwtService.generateRefreshToken(user);
+
+        return new LoginResponseDto(user.getId(), accessToken, refreshToken);
+    }
+
+    public LoginResponseDto refreshToken(String refreshToken){
+        Long userId = jwtService.getUserIdFromToken(refreshToken);
+        User user = userService.getUserById(userId);
+
+        String accessToken =  jwtService.generateAccessToken(user);
+        return new LoginResponseDto(user.getId(), accessToken, refreshToken);
     }
 }
