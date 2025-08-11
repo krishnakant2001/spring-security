9cc6939616efe0a11eb477625e797fdce2ec8274
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2025-08-11 09:56:07 +0530

    User sessions management with JWT

---
+++ src/main/java/com/krishnakant/SecurityApp/SecurityApplication/repositories/SessionRepository.java
@@ -0,0 +1,14 @@
+package com.krishnakant.SecurityApp.SecurityApplication.repositories;
+
+import com.krishnakant.SecurityApp.SecurityApplication.entities.Session;
+import com.krishnakant.SecurityApp.SecurityApplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByUser(User user);
+
+    Optional<Session> findByRefreshToken(String refreshToken);
+}
